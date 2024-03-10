5263c4dee65ea0ab795afec72f94144b4553dd61
Guilherme Pereira <guilospanck@protonmail.com>
2024-03-10 09:58:18 +0000

    Add function to map from target to bits

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,13 @@
+
+pub const MAXIMUM_TARGET_EXPONENT: u8 = 0x1d;
+pub const MAXIMUM_TARGET_COEFFICIENT: [u8; 3] =  [0x00, 0xff, 0xff];
 pub const MAXIMUM_TARGET_COMPRESSED: i32 = 0x1d00ffff;
+
+pub const UNCOMPRESSED_LENGTH_IN_BYTES: usize = 32;
 /// 0x00000000ffff0000000000000000000000000000000000000000000000000000
-pub const MAXIMUM_TARGET_UNCOMPRESSED: [u8; 32] = [0, 0, 0, 0, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+pub const MAXIMUM_TARGET_UNCOMPRESSED: [u8; UNCOMPRESSED_LENGTH_IN_BYTES] = [0, 0, 0, 0, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 pub const MINIMUM_DIFFICULTY: i32 = 1;
+
+/// To avoid setting a negative number. See https://learnmeabitcoin.com/technical/block/bits/
+pub const MAXIMUM_FIRST_SIGNIFICANT_BYTE: u8 = 128; // 0x80
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 mod constants;
 
-use crate::constants::{MAXIMUM_TARGET_COMPRESSED, MAXIMUM_TARGET_UNCOMPRESSED, MINIMUM_DIFFICULTY};
+use crate::constants::{
+  MAXIMUM_FIRST_SIGNIFICANT_BYTE, MAXIMUM_TARGET_COEFFICIENT, MAXIMUM_TARGET_COMPRESSED,
+  MAXIMUM_TARGET_EXPONENT, MAXIMUM_TARGET_UNCOMPRESSED, MINIMUM_DIFFICULTY,
+  UNCOMPRESSED_LENGTH_IN_BYTES,
+};
+
+use hex;
+
+#[derive(Debug)]
+struct Bits {
+  pub value: i32,
+  pub exponent: u8,
+  pub coefficient: Vec<u8>,
+}
+
+impl Bits {
+  fn new() -> Self {
+    Bits {
+      value: MAXIMUM_TARGET_COMPRESSED,
+      exponent: MAXIMUM_TARGET_EXPONENT,
+      coefficient: MAXIMUM_TARGET_COEFFICIENT.to_vec(),
+    }
+  }
+}
 
 struct Target {
   /// This is a 4-byte (32 bits) value
   /// It is known as BITS.
-  compressed: i32,
+  compressed: Bits,
   /// This is a 32-byte (256 bits) value
   uncompressed: Vec<u8>,
   /// The difficulty is a human-readable number that helps understand
@@ -14,16 +37,66 @@ struct Target {
 }
 
 impl Target {
-  fn new() -> Target {
+  fn new() -> Self {
     Target {
-      compressed: MAXIMUM_TARGET_COMPRESSED,
+      compressed: Bits::new(),
       uncompressed: MAXIMUM_TARGET_UNCOMPRESSED.to_vec(),
       difficulty: MINIMUM_DIFFICULTY,
     }
   }
 
-  fn to_bits(self) -> i32 {
-    0
+  fn to_bits(&self) -> Bits {
+    // get first three signficant bytes and the index of the first signficant byte
+    let (coefficient, byte_index) = self._get_coefficient_and_byte_index();
+    let exponent = self._get_exponent(byte_index);
+
+    let mut value_as_vec = vec![exponent];
+    value_as_vec.append(&mut coefficient.clone());
+
+    let as_array: [u8; 4] = value_as_vec[..]
+      .try_into()
+      .unwrap_or_else(|e| panic!("Could not transform vector into array of size 4: {}", e));
+
+    let value = i32::from_be_bytes(as_array);
+
+    Bits {
+      coefficient,
+      exponent,
+      value,
+    }
+  }
+
+  fn _get_coefficient_and_byte_index(&self) -> (Vec<u8>, usize) {
+    let mut coefficient_bytes: Vec<u8> = vec![];
+    let mut byte_index: usize = 0;
+
+    for (index, byte) in self.uncompressed.iter().enumerate() {
+      if coefficient_bytes.len() == 3 {
+        break;
+      }
+
+      if *byte == 0u8 {
+        continue;
+      }
+
+      if *byte >= MAXIMUM_FIRST_SIGNIFICANT_BYTE && coefficient_bytes.len() == 0 {
+        byte_index = index;
+        coefficient_bytes.push(0);
+      }
+
+      if coefficient_bytes.len() == 0 {
+        byte_index = index;
+      }
+
+      coefficient_bytes.push(*byte);
+    }
+
+    (coefficient_bytes, byte_index)
+  }
+
+  fn _get_exponent(&self, byte_index: usize) -> u8 {
+    // The index starts at 0, so we must add 1 to offset it
+    (UNCOMPRESSED_LENGTH_IN_BYTES - byte_index + 1) as u8
   }
 
   fn _check_if_uncompressed_target_is_valid(self) -> bool {
@@ -38,5 +111,12 @@ impl Target {
 fn main() {
   let target = Target::new();
 
-  println!("{}", target._check_if_uncompressed_target_is_valid());
+  let bits = target.to_bits();
+
+  let encoded_bits_value = hex::encode(bits.value.to_be_bytes());
+
+
+  println!("{:?}", bits.coefficient);
+  println!("{:?}", bits.exponent);
+  println!("{:?}", encoded_bits_value);
 }
